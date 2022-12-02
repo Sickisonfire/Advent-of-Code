1ddf17faaf9d646aa260846301ffb08b527669f9
Marcel <firebird10@gmx.de>
2022-12-02 15:08:59 +0100

    feat(2022): day1 p1

--- 2022/src/days/one.rs
+++ 2022/src/days/one.rs
@@ -1,4 +1,28 @@
 pub fn solve() {
-    println!("solve day 1");
+    let input = include_str!("../../inputs/input_one");
+    part_one(input);
+    part_two(input);
 }
 
+fn part_one(input: &str) {
+    let iter: Vec<&str> = input.split("\n").collect();
+    let mut sums = Vec::new();
+    let mut sum = 0;
+
+    for entry in iter {
+        match entry {
+            "" => {
+                sums.push(sum);
+                sum = 0;
+            }
+            n => {
+                sum += n.parse::<usize>().unwrap_or(0);
+            }
+        }
+    }
+    println!("max is {}", sums.iter().max().unwrap());
+}
+
+fn part_two(input: &str) {
+    for entry in input.split('\n') {}
+}
