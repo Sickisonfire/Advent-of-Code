496e12d074de078720a07d17ead6d785bef1c878
Marcel <firebird10@gmx.de>
2023-06-11 21:09:40 +0200

    2020: Day 9 Part 2

--- 2020/src/9/index.ts
+++ 2020/src/9/index.ts
@@ -7,11 +7,30 @@ export default () => {
   let rawString = fs.readFileSync(inputPath).toString().slice(0, -1)
   let lines = rawString.split("\n").map(l => l.split(""))
 
-  let total = 0
+  partOne(lines)
+  partTwo(lines)
+
+}
+
+const getCharPrio = (char: string) => {
+
   let encoder = new TextEncoder()
   let encodedValueOffset = [96, 38]
+  let charPrio = 0
 
   let isUpperCase = (s: string) => /^[A-Z]$/.test(s)
+
+  if (isUpperCase(char)) {
+    charPrio = encoder.encode(char)[0] - encodedValueOffset[1]
+  } else {
+    charPrio = encoder.encode(char)[0] - encodedValueOffset[0]
+  }
+  return charPrio
+}
+
+const partOne = (lines: string[][]) => {
+
+  let total = 0
 
   for (const line of lines) {
     const s = new Set()
@@ -24,15 +43,40 @@ export default () => {
     l.forEach(el => s.add(el))
     r.forEach(el => { if (s.has(el)) char = el })
 
-    let charPrio = 0
-    if (isUpperCase(char)) {
-      charPrio = encoder.encode(char)[0] - encodedValueOffset[1]
-    } else {
-      charPrio = encoder.encode(char)[0] - encodedValueOffset[0]
-    }
+    let charPrio = getCharPrio(char)
     total += charPrio
-    console.log(charPrio)
   }
-  console.log(total)
+  console.log("Part 1:", total)
+}
 
+const partTwo = (lines: string[][]) => {
+  let lineCounter = 1
+
+  let total = 0
+
+  let s = new Map()
+  for (const line of lines) {
+    switch (lineCounter) {
+      case 1:
+        s.clear()
+
+        line.forEach(el => s.set(el, false))
+        lineCounter += 1
+        break
+
+      case 2:
+        line.forEach(el => { if (s.has(el)) s.set(el, true) })
+        lineCounter += 1
+        break
+      case 3:
+        let char = ""
+        line.forEach(el => { if (s.get(el) === true) char = el })
+        total += getCharPrio(char)
+        lineCounter = 1
+        break
+      default:
+        break
+    }
+  }
+  console.log("Part 2:", total)
 }
