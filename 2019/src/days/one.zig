d43dde42d95d5c4f27c9d6313f2fed55fd7b9e3f
Marcel <firebird10@gmx.de>
2023-06-20 16:29:41 +0200

    2019: day one part two

--- 2019/src/days/one.zig
+++ 2019/src/days/one.zig
@@ -16,13 +16,35 @@ pub fn solve(allocator: std.mem.Allocator) !void {
 
     var lines_iter = std.mem.splitSequence(u8, file_buf, "\n");
 
-    var sum: i32 = 0;
+    var sum_p1: i32 = 0;
+    var sum_p2: i32 = 0;
     while (lines_iter.next()) |line| {
         if (line.len != 0) {
             var line_int = try std.fmt.parseInt(i32, line, 0);
-            var module_fuel = @floatToInt(i32, @intToFloat(f32, line_int) / 3.0) - 2;
-            sum += module_fuel;
+            var fuel_p1 = calc_fuel(line_int);
+            var fuel_p2 = calc_fuel_rec(line_int, 0);
+            sum_p1 += fuel_p1;
+            sum_p2 += fuel_p2;
         }
     }
-    std.debug.print("{d} ", .{sum});
+    std.debug.print("Part one: {d} \n", .{sum_p1});
+    std.debug.print("Part two: {d} \n", .{sum_p2});
+}
+
+// recurson like this may result in undefined behaviour
+fn calc_fuel_rec(weight: i32, acc: i32) i32 {
+    var total: i32 = acc;
+    if (weight == 0) return total;
+
+    var fuel: i32 = calc_fuel(weight);
+    total += fuel;
+
+    return calc_fuel_rec(fuel, total);
+}
+
+fn calc_fuel(weight: i32) i32 {
+    // zero or negative cases
+    if (weight <= 8) return 0;
+
+    return @floatToInt(i32, @intToFloat(f32, weight) / 3.0) - 2;
 }
